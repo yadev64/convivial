4f1a76a8f236b2017f7db3cd3f1d7ccae7534fba
YadevJayachandran <yadev64@gmail.com>
2021-05-26 02:40:44 +0530

    User authentication using Sanctum
    - User registration & login authentication created at the api

---
+++ convivial_api/convivial_api/app/Models/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class admin extends Model
+{
+    use HasFactory;
+}
--- convivial_api/convivial_api/routes/api.php
+++ convivial_api/convivial_api/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,9 +13,18 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/home', [AuthController::class, 'home']);
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
