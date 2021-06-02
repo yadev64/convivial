34bbb7ccd8baa227a2b506842ffefd98770b78f3
YadevJayachandran <yadev64@gmail.com>
2021-06-02 21:39:16 +0530

    Bug fix
    - Admin registration validation
    - Login validation at backend

--- convivial_api/convivial_api/app/Http/Controllers/AuthController.php
+++ convivial_api/convivial_api/app/Http/Controllers/AuthController.php
@@ -10,30 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
 
-    public function register(Request $request)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
-
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-        ]);
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
-    }
-
-
-    public function login(Request $request)
+    public static function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
         return response()->json([
@@ -49,8 +26,57 @@ class AuthController extends Controller
         ]);
     }
 
+
+    public function register(Request $request)
+    {
+        if (User::where('email', $request->email)->exists()) {
+            return AuthController::login($request);
+        }
+
+        if($request->password = null || strlen($request->password) < 8){
+            return response()->json([
+                'message' => 'Password should have minimum 8 characters'], 411);
+        }
+
+        if(!AuthController::validateEmail($request->email)){
+            return response()->json([
+                'message' => 'Invalid email'], 406);
+        }
+
+        if($request->name == null || strlen($request->name) < 3){
+            echo $request->name;
+            return response()->json([
+                'message' => 'Invalid username'], 406);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // echo json_encode($validatedData);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
     public function me(Request $request)
     {
         return $request->user();
+    }
+
+    static function validateEmail($str) {
+        return (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
     }
 }
